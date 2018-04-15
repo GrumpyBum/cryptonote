53ede87bf0a991b4bc0dcebd7eb37618a68f96b2
Jofta <office@jofta.com>
2018-04-15 10:45:00 +0000

    Daemon JSON RPC API updated

--- src/BlockchainExplorer/BlockchainExplorerDataBuilder.h
+++ src/BlockchainExplorer/BlockchainExplorerDataBuilder.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2011-2017, The ManateeCoin Developers, The Cryptonote developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -26,11 +26,11 @@ public:
 
   bool fillBlockDetails(const Block& block, BlockDetails& blockDetails);
   bool fillTransactionDetails(const Transaction &tx, TransactionDetails& txRpcInfo, uint64_t timestamp = 0);
+  bool getMixin(const Transaction& transaction, uint64_t& mixin);
 
   static bool getPaymentId(const Transaction& transaction, Crypto::Hash& paymentId);
 
 private:
-  bool getMixin(const Transaction& transaction, uint64_t& mixin);
   bool fillTxExtra(const std::vector<uint8_t>& rawExtra, TransactionExtraDetails& extraDetails);
   size_t median(std::vector<size_t>& v);
 
@@ -38,3 +38,4 @@ private:
   CryptoNote::ICryptoNoteProtocolQuery& protocol;
 };
 }
+
